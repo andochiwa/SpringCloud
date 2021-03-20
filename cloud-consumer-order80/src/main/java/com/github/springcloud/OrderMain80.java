cdf287934b40e00cd9ae4d86cf46fef43f7f34a8
andochiwa <a1066079469@gmail.com>
2021-03-20 22:25:15 +0900

    consumer-order

---
+++ cloud-consumer-order80/src/main/java/com/github/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.github.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 03-20-22:24
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args){
+        SpringApplication.run(OrderMain80.class, args);
+    }
+
+}
