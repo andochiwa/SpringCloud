ecbd7f4e808bee5ac7a5c5cd2554ce1c9a7d1682
andochiwa <a1066079469@gmail.com>
2021-03-23 19:54:23 +0900

    Bus

--- cloud-config-client3355/src/main/java/com/github/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/github/springcloud/controller/ConfigClientController.java
@@ -1,7 +1,7 @@
 package com.github.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,19 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ConfigClientController {
-    @Value("${info.date}")
-    private String date ;
-    @Value("${info.author}")
-    private String author ;
-    @Value("${info.sign}")
-    private String sign ;
-    @Value("${info.version}")
-    private String version ;
-    /**
-     * 获取配置信息
-     */
-    @RequestMapping("/getConfigInfo")
-    public String getConfigInfo (){
-        return date+"-"+author+"-"+sign+"-"+version ;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
     }
+
 }
